b134c5c89be0e9e3f4386d6bcbc3482b7137b079
holmofy <hff1996723@163.com>
2024-10-15 13:14:03 +0800

    feat: :sparkles: raw include_http

--- examples/include_http.rs
+++ examples/include_http.rs
@@ -1,3 +1,7 @@
 use reqwest_scraper::include_http;
 
 include_http!("examples/example.http");
+
+fn main(){
+    
+}
--- reqwest-scraper-macros/src/include_http.rs
+++ reqwest-scraper-macros/src/include_http.rs
@@ -4,6 +4,7 @@ use lazy_static::lazy_static;
 use proc_macro2::{Span, TokenStream};
 use quote::{quote, ToTokens};
 use regex::Regex;
+use syn::Ident;
 
 pub fn expand_macro(file_path: String) -> syn::Result<TokenStream> {
     let http_content = std::fs::read_to_string(&file_path).map_err(move |e| {
@@ -14,15 +15,16 @@ pub fn expand_macro(file_path: String) -> syn::Result<TokenStream> {
     })?;
 
     let requests = parse_http_file(&http_content);
-
-    Ok(quote!{#(#requests)*})
+    eprintln!("request count: {}", requests.len());
+    Ok(quote! {#(#requests)*})
 }
 
 lazy_static! {
     // 正则表达式解析 HTTP 方法和 URL
     static ref SNIPPET_SPLITTER: Regex = Regex::new(r"#{3,}").unwrap();
-    static ref HTTP_RE: Regex = Regex::new(r"###\s*(?<name>\w+)\n(?<method>GET|POST|HEAD|PUT|DELETE|PATCH|HEAD|OPTIONS|TRACE)\s*(?<url>https?://\S+)\n(?<headers>(?:\S+:\s+[^\n]+\n)*)\n?(?<body>[\s\S]*)").unwrap();
+    static ref HTTP_RE: Regex = Regex::new(r"(?<name>\w+)\n(?<method>GET|POST|HEAD|PUT|DELETE|PATCH|HEAD|OPTIONS|TRACE)\s*(?<url>https?://\S+)(?:\n(?<headers>(?:\S+:\s+[^\n]+\n)*)\n?(?<body>[\s\S]*))?").unwrap();
     static ref HEADER_RE: Regex = Regex::new(r"(?<key>\S+):\s*(?<value>[^\n]+)").unwrap();
+    static ref VARIABLE_RE: Regex = Regex::new(r"{{(?<ident>\w+)(?::\s*(?<ty>\w+))?}}").unwrap();
 }
 
 fn parse_http_file(content: &str) -> Vec<HttpRequestFn> {
@@ -30,17 +32,20 @@ fn parse_http_file(content: &str) -> Vec<HttpRequestFn> {
     snippets
         .into_iter()
         .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
         .flat_map(parse_http)
         .collect()
 }
 
 fn parse_http(content: &str) -> Vec<HttpRequestFn> {
     let mut request_fns = vec![];
+    eprintln!("content: {content}");
     for caps in HTTP_RE.captures_iter(content) {
         let name = caps.name("name").unwrap().as_str();
         let method = caps.name("method").unwrap().as_str();
         let url = caps.name("url").unwrap().as_str();
 
+        eprintln!(">>>pared>>>>>{name}: {method} {url}");
         let mut headers = HashMap::new();
         if let Some(headers_str) = caps.name("headers") {
             for caps in HEADER_RE.captures_iter(headers_str.as_str()) {
@@ -80,9 +85,9 @@ struct HttpRequest<'f> {
 impl<'f> ToTokens for HttpRequestFn<'f> {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let Self { name, request } = self;
+        let method_name_ident = Ident::new(name, Span::call_site());
         tokens.extend(quote! {
-            pub async fn #name() -> Result<reqwest::Response, reqwest::Error> {
-                let client = reqwest::Client::new();
+            pub async fn #method_name_ident() -> Result<reqwest::Response, reqwest::Error> {
                 #request
             }
         });
@@ -98,13 +103,14 @@ impl<'f> ToTokens for HttpRequest<'f> {
             body,
         } = self;
         let method = method.to_lowercase();
-        tokens.extend(quote! {client.#method(#url)});
+        let method_ident = Ident::new(&method, Span::call_site());
+        tokens.extend(quote! {reqwest::Client::new().#method_ident(#url)});
         for (key, value) in headers {
             tokens.extend(quote! {.header(#key, #value)});
         }
         if let Some(body) = body {
             tokens.extend(quote! {.body(#body)});
         }
-        tokens.extend(quote!{.send().await})
+        tokens.extend(quote! {.send().await})
     }
 }
