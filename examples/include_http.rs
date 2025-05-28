f7dc77a21838fb940e1988b3edb411ac3f9f2d86
holmofy <hff1996723@163.com>
2025-05-29 00:02:59 +0800

    fix build error

--- examples/include_http.rs
+++ examples/include_http.rs
@@ -1,8 +1,13 @@
-use reqwest_middleware::ClientWithMiddleware;
+use reqwest::Client;
+// use reqwest_middleware::ClientWithMiddleware;
 use reqwest_scraper::{error::Result, include_http, ScraperResponse};
 
-fn client() -> ClientWithMiddleware {
-    ClientWithMiddleware::default()
+// fn client() -> ClientWithMiddleware {
+//     ClientWithMiddleware::default()
+// }
+
+fn client() -> Client {
+    Client::default()
 }
 
 include_http!("examples/example.http", client, {phone="18720232389", password="101010"});
